766bf8264c87dfa2b8e0b4056b7e703e76d4376c
Yokkin <76399182+froggie3@users.noreply.github.com>
2023-01-14 23:14:31 +0900

    added option

--- stable_diffusion_setup.php
+++ stable_diffusion_setup.php
@@ -6,18 +6,28 @@ declare(strict_types=1);
 require_once __DIR__ . '/vendor/autoload.php';
 
 /*
- * オプション用の関数
+ * unlink か link かを決定するオプション用の関数
  */
 
-$options = getopt('hr', ['unlink', 'json:']);
+$options = getopt('hr', ['unlink', 'json:', 'symlink']);
 
-function determine_operation(array &$options): int
+function determine_is_unlink(array &$options): int
 {
-    $Option = new classes\Option;
-    return $Option->is_unlink($options);
+    return (new classes\Option)->is_unlink($options);
 }
 
-(int) $is_unlink = determine_operation($options);
+$is_unlink = determine_is_unlink($options);
+
+/*
+ * link か symlink かを決定するオプション用の関数
+ */
+
+ function determine_has_symlink(array &$options): bool
+ {
+    return (new classes\Option)->has_symlink($options);
+ }
+ 
+$has_symlink = determine_has_symlink($options);
 
 function determine_config_parameters(array &$options): array
 {
@@ -67,7 +77,8 @@ foreach ($Config->cfg_array as $item) {
             $Execute->execute(
                 $sourcedir . '/' . $filename,
                 $Config->webui_dir . '/models/Stable-diffusion/' . $filename,
-                $is_unlink
+                $is_unlink,
+                $has_symlink
             );
         }
     }
@@ -80,7 +91,8 @@ foreach ($Config->cfg_array as $item) {
             $Execute->execute(
                 $sourcedir . '/' . $filename,
                 $Config->webui_dir . '/models/VAE/' . $filename,
-                $is_unlink
+                $is_unlink,
+                $has_symlink
             );
         }
     }
@@ -93,7 +105,8 @@ foreach ($Config->cfg_array as $item) {
             $Execute->execute(
                 $sourcedir . '/' . $filename,
                 $Config->webui_dir . '/embeddings/' . $filename,
-                $is_unlink
+                $is_unlink,
+                $has_symlink
             );
         }
     }
@@ -106,7 +119,8 @@ foreach ($Config->cfg_array as $item) {
             $Execute->execute(
                 $sourcedir . '/' . $filename,
                 $Config->webui_dir . '/models/hypernetworks/' . $filename,
-                $is_unlink
+                $is_unlink,
+                $has_symlink
             );
         }
     }
@@ -123,7 +137,8 @@ foreach ($Config->cfg_array as $item) {
             $Execute->execute(
                 $dir_prefix . '/' . $filename,
                 $Config->webui_dir . '/models/hypernetworks/' . $filename,
-                $is_unlink
+                $is_unlink,
+                $has_symlink
             );
         }
         unset($Path);
